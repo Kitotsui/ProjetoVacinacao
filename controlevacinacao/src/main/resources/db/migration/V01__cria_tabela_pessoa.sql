5382642839a9a87216cc9341d224b96c956a7e75
Kitotsui <rogerio_c.o.jr@outlook.com>
2024-10-22 21:14:04 -0300

    Começando a trabalhar no CRUD pessoa

---
+++ controlevacinacao/src/main/resources/db/migration/V01__cria_tabela_pessoa.sql
@@ -0,0 +1,10 @@
+CREATE TABLE public.pessoa
+(
+    codigo bigserial NOT NULL,
+    nome text,
+    cpf text,
+    data_nascimento_de date,
+    data_nascimento_ate date,
+    profissao text,
+    PRIMARY KEY (codigo)
+);
